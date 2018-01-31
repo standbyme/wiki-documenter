12989356a302fc9e5dd8f4650601cf76486c20e4
TGS <tgs@tgs.blue>
2018-01-31 16:25:57 +0800

    remove space in url

--- src/related_link__set_utility.ts
+++ src/related_link__set_utility.ts
@@ -6,6 +6,6 @@ export function URL_constructor(entity: EntityInterface): string {
     const { name, modifier__set } = entity
     const keyword__list = _.concat(Array.from(modifier__set), name)
     const query = encodeURIComponent(_.join(keyword__list, ' '))
-    const result = `https:// www.baidu.com/s?wd=site%3A(baike.baidu.com)%20title%3A%20(${query})`
+    const result = `https://www.baidu.com/s?wd=site%3A(baike.baidu.com)%20title%3A%20(${query})`
     return result
 }
--- test/related_link__set.ts
+++ test/related_link__set.ts
@@ -12,7 +12,7 @@ describe('Related Link Set', function () {
     describe('Utility', function () {
         it('URL_constructor', function () {
             const result = URL_constructor(mock)
-            assert.equal(result, 'https:// www.baidu.com/s?wd=site%3A(baike.baidu.com)%20title%3A%20(%E4%B8%AD%E5%8D%8E%20%E5%A4%96%E4%BA%A4%E9%83%A8%20%E7%8E%8B%E6%AF%85)')
+            assert.equal(result, 'https://www.baidu.com/s?wd=site%3A(baike.baidu.com)%20title%3A%20(%E4%B8%AD%E5%8D%8E%20%E5%A4%96%E4%BA%A4%E9%83%A8%20%E7%8E%8B%E6%AF%85)')
         })
     })
 })
