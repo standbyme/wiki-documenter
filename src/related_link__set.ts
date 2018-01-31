caca2bf9c2e14a294b0a2cdb4e15ad288bb3412b
TGS <tgs@tgs.blue>
2018-01-31 18:38:07 +0800

    related_link__set

--- src/related_link__set.ts
+++ src/related_link__set.ts
@@ -1,8 +1,22 @@
 import axios from 'axios'
 import * as cheerio from 'cheerio'
+import { Option } from 'funfix-core'
 
 import { EntityInterface } from './interface'
 import { URL_constructor } from './related_link__set_utility'
 
-export default function (entity: EntityInterface) {
+export default async function (entity: EntityInterface): Promise<Option<Set<string>>> {
+    const url = URL_constructor(entity)
+
+    const { data } = await axios.get(url)
+    const $ = cheerio.load(data)
+    const result = $('#content_left').find('h3').map(function () {
+        return $(this).children().first().attr('href')
+    }).get()
+    const len = result.length
+    if (len) {
+        return Option.of(new Set(result))
+    } else {
+        return Option.none()
+    }
 }
--- test/related_link__set.ts
+++ test/related_link__set.ts
@@ -1,18 +1,44 @@
 import * as assert from 'assert'
 
 import { EntityInterface } from '../src/interface'
+import related_link__set from '../src/related_link__set'
 import { URL_constructor } from '../src/related_link__set_utility'
 
 describe('Related Link Set', function () {
-    const mock: EntityInterface = {
-        name: '王毅',
-        modifier__set: new Set(['中华', '外交部'])
-    }
 
     describe('Utility', function () {
         it('URL_constructor', function () {
+            const mock: EntityInterface = {
+                name: '王毅',
+                modifier__set: new Set(['中华', '外交部'])
+            }
             const result = URL_constructor(mock)
-            assert.equal(result, 'https://www.baidu.com/s?wd=site%3A(baike.baidu.com)%20title%3A%20(%E4%B8%AD%E5%8D%8E%20%E5%A4%96%E4%BA%A4%E9%83%A8%20%E7%8E%8B%E6%AF%85)')
+            assert.equal(result, 'http://www.baidu.com/s?wd=site%3A(baike.baidu.com)%20title%3A%20(%E4%B8%AD%E5%8D%8E%20%E5%A4%96%E4%BA%A4%E9%83%A8%20%E7%8E%8B%E6%AF%85)')
+        })
+    })
+
+    describe('Main', function () {
+        this.slow(5000)
+        this.timeout(10000)
+        const mock: EntityInterface = {
+            name: '王毅',
+            modifier__set: new Set(['银行'])
+        }
+
+        const wrong_mock: EntityInterface = {
+            name: '王毅',
+            modifier__set: new Set(['正则表达式'])
+        }
+        it('should return Some when entity exists', async function () {
+            const result_opt = await related_link__set(mock)
+            assert(result_opt.nonEmpty())
+            const result = result_opt.get()
+            assert.equal(result.size, 2)
+        })
+
+        it('should return None when entity does not exist', async function () {
+            const result_opt = await related_link__set(wrong_mock)
+            assert(result_opt.isEmpty())
         })
     })
 })
