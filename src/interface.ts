a71c164cfdaea4431c62df02c742f3acceeed511
TGS <tgs@tgs.blue>
2018-01-31 22:45:36 +0800

    fix cheerio to CheerioStatic

--- src/interface.ts
+++ src/interface.ts
@@ -1,7 +1,7 @@
 import * as Cheerio from 'cheerio'
 import { Try } from 'funfix-core'
 interface ProjectInterface {
-    localizer(original_html: Cheerio): Try<Cheerio>
+    localizer(original_html: CheerioStatic): Try<Cheerio>
     // tslint:disable-next-line:no-any
     parser(html: Cheerio): Try<any>
 }
